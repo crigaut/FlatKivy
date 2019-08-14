fd60dc3d9f6df85d73a3cedcf0191d0e8cea0e2d
crigaut <clement.rigaut@outlook.com>
2019-08-14 15:17:20 +0200

    Changed bytes strings to Unicode strings

--- flat_kivy/uix/behaviors.py
+++ flat_kivy/uix/behaviors.py
@@ -164,7 +164,7 @@ class ButtonBehavior(object):
                             'touches', touch_id,
                             'button_pressed', self.text, do_timestamp=True)
             self._do_press()
-            self.dispatch(b'on_press')
+            self.dispatch('on_press')
         return super(ButtonBehavior, self).on_touch_down(touch)
 
     def on_touch_move(self, touch):
@@ -187,7 +187,7 @@ class ButtonBehavior(object):
                             'touches', touch_id, 'button_released',
                             self.text, do_timestamp=True)
             self._do_release()
-            self.dispatch(b'on_release')
+            self.dispatch('on_release')
         return super(ButtonBehavior, self).on_touch_up(touch)
 
     def on_press(self):
